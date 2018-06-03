cad2f179570cae333fc99e91b8da20ac269af380
Johannes Jacobs <johannes-jacobs@live.de>
2018-06-03 18:34:16 +0200

    Project Commit

---
+++ BF3Tweaks/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace BF3Tweaks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TMem.Instance)
+            {
+                if(TMem.Instance.TryOpen("bf3"))
+                {
+                    bool killLoop = false;
+                    while(!killLoop)
+                    {
+                        var pGameContext = TMem.Instance.Read<IntPtr>(new IntPtr(0x2380b58));
+                        var pClientLevel = TMem.Instance.Read<IntPtr>(pGameContext + 0x10);
+                        var pWorldRenderModule = TMem.Instance.Read<IntPtr>(pClientLevel + 0xB8);
+                        var pWorldRenderer = TMem.Instance.Read<IntPtr>(pWorldRenderModule + 24);
+                        var pWorldRenderSettings = TMem.Instance.Read<IntPtr>(pWorldRenderer + 0x8988);
+
+                        Console.WriteLine("(1): Toggle Sun");
+                        Console.WriteLine("(2): Toggle Sky");
+                        Console.WriteLine("(0): Exit");
+
+                        Console.WriteLine();
+                        Console.Write("$> ");
+                        if (int.TryParse(Console.ReadLine(), out var value))
+                        {
+                            switch (value)
+                            {
+                                case 1:
+                                    TMem.Instance.Write(pWorldRenderSettings + 0x1BE, !(TMem.Instance.Read<bool>(pWorldRenderSettings + 0x1BE)));
+                                    break;
+                                case 2:
+                                    TMem.Instance.Write(pWorldRenderSettings + 0x1B7, !(TMem.Instance.Read<bool>(pWorldRenderSettings + 0x1B7)));
+                                    break;
+                                case 3:
+                                default:
+                                    killLoop = true;
+                                    continue;
+                            }
+
+                            Console.Clear();
+                            Console.WriteLine("Success");
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to obtain handle, please run the game first or make sure you are running as administrator.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ BF3Tweaks/TMem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BF3Tweaks
+{
+    public class TMem : IDisposable
+    {
+        private static TMem instance;
+        public static TMem Instance
+        {
+            get
+            {
+                return instance == null ? (instance = new TMem()) : instance;
+            }
+        }
+
+        public IntPtr ProcessHandle { get; private set; }
+
+        public bool TryOpen(string processName, WinApi.ProcessAccessFlags desiredAccess = WinApi.ProcessAccessFlags.All)
+        {
+            var processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0)
+                return false;
+
+            try
+            {
+                ProcessHandle = WinApi.OpenProcess(processes[0], desiredAccess);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool TryClose()
+        {
+            if (ProcessHandle == IntPtr.Zero)
+                return false;
+
+            return WinApi.CloseHandle(ProcessHandle);
+        }
+
+        public T Read<T>(in IntPtr address) where T : new()
+        {
+            var obj = new T();
+            var size = Marshal.SizeOf(obj);
+            var buf = new byte[size];
+
+            if (!WinApi.ReadProcessMemory(ProcessHandle, address, buf, size, out var bRead))
+                return default;
+
+            var handle = GCHandle.Alloc(buf, GCHandleType.Pinned);
+
+            obj = Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+
+            handle.Free();
+
+            return obj;
+        }
+
+        public void Write<T>(in IntPtr address, T value) where T : new()
+        {
+            var size = Marshal.SizeOf<T>();
+            var buffer = new byte[size];
+
+            var ptr = Marshal.AllocHGlobal(size);
+
+            Marshal.StructureToPtr(value, ptr, true);
+            Marshal.Copy(ptr, buffer, 0, size);
+            Marshal.FreeHGlobal(ptr);
+
+            WinApi.WriteProcessMemory(ProcessHandle, address, buffer, buffer.Length, out var bytesWritten);
+        }
+
+        public void Dispose()
+        {
+            TryClose();
+        }
+    }
+}
